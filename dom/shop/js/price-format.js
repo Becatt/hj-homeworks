a1f29b9127ce929b561f19a3e31cbe247f867540
Sergey Beketov <beketov.serg@gmail.com>
2018-12-27 00:00:17 +0300

    Решение 2-ой задачи

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,19 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+let cartCount = 0;
+let totalPrice = 0;
+function SumPrice(event) {
+   totalPrice += parseInt(this.dataset.price)
+   document.querySelector('#cart-total-price').innerHTML = getPriceFormatted(totalPrice);
+   cartCount++;
+   document.querySelector('#cart-count').innerHTML = cartCount;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btns = document.querySelectorAll('button.add');
+  for(let btn of btns) {
+    btn.addEventListener('click', SumPrice);
+  }
+});
