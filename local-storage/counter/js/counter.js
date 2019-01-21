b81080baf4529bb0f1588701c3fe229bc7c5d895
Sergey Beketov <beketov.serg@gmail.com>
2019-01-21 22:36:10 +0300

    решение задачи 1

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,41 @@
 'use strict';
+
+let count = 0;
+
+const container = document.querySelector('.wrap-btns');
+const counter = document.querySelector('#counter');
+
+container.addEventListener('click', setCount);
+showCount();
+
+function getCoockieCount() {
+  if(document.cookie) {
+    const cookieData = document.cookie.split(';').forEach(el => {
+      const split = el.split('=');
+      if(split[0] === 'count') {
+        count = split[1];
+      }
+    });
+  }
+}
+
+function showCount() {
+  getCoockieCount();
+  counter.textContent = count;
+}
+
+function setCount(event) {
+  switch(event.target.getAttribute('id')) {
+    case 'increment':
+      count++;
+      break;
+    case 'decrement':
+      count--;
+      break;
+    case 'reset':
+      count = 0;
+      break;
+  }
+  document.cookie = `count=${count}`
+  counter.textContent = count;
+}
