9768d241ef7f57fde2d10a2047f9bbda9c0b16c8
Sergey Beketov <beketov.serg@gmail.com>
2019-01-28 23:49:21 +0300

    решение задач 2 и 3

--- websocket/chat/js/chat.js
+++ websocket/chat/js/chat.js
@@ -1,1 +1,70 @@
 'use strict';
+
+const chat = document.querySelector('.chat');
+const messagesTemplates = document.querySelector('.messages-templates');
+const chatStatus = chat.querySelector('.chat-status');
+const messageStatus = messagesTemplates.querySelector('.message-status');
+const messages = chat.querySelector('.messages-content')
+const submit = chat.querySelector('.message-submit');
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/chat');
+
+function addNode(node, text = '', selector = '.message-text') {
+  const clone = node.cloneNode(true);
+  messages.appendChild(clone);
+  const lastChild = messages.children[messages.children.length - 1];
+  if (text !== '') {
+    lastChild.querySelector(selector).textContent = text;
+  }
+  if(lastChild.querySelector('.timestamp')) {
+    const now = new Date();
+    lastChild.querySelector('.timestamp').textContent = now.toLocaleTimeString().slice(0, 5);
+  }
+}
+
+connection.onopen = () => {
+  chatStatus.textContent = chatStatus.getAttribute('data-online');
+  addNode(messageStatus, 'Пользователь появился в сети')
+  submit.removeAttribute('disabled');
+}
+
+connection.onmessage = (event) => {
+  const data = event.data;
+  if(data === '...') {
+    addNode(messagesTemplates.querySelector('.loading'));
+
+  } else {
+    if(messages.querySelector('.loading')) {
+      messages.removeChild(messages.querySelector('.loading'));
+    }
+    addNode(messagesTemplates.querySelectorAll('.message')[1], data);
+  }
+
+}
+
+submit.addEventListener('click', sendMessage);
+
+function sendMessage(event) {
+  event.preventDefault();
+  const text = chat.querySelector('.message-input').value;
+  addNode(messagesTemplates.querySelector('.message-personal'), text);
+  connection.send(text);
+  chat.querySelector('.message-input').value = '';
+}
+
+connection.onclose = () => {
+  chatStatus.textContent = chatStatus.getAttribute('data-offline');
+  addNode(messageStatus);
+  submit.setAttribute('disabled', '');
+}
+
+// Разрыв соединения сочетание клавиш ctrl+Q
+document.addEventListener('keydown', closeConnection);
+function closeConnection(event) {
+  if (!event.ctrlKey) {
+    return;
+  }
+  if(event.code === 'KeyQ') {
+    connection.close();
+  }
+}
--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  console.log(data);
+  document.querySelector('.counter').textContent = data.connections;
+  document.querySelector('output.errors').textContent = data.errors;
+}
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000);
+});
