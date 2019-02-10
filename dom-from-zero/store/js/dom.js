53173314d9a5e127d7ca924fe304ed4f6e71e89a
Sergey Beketov <beketov.serg@gmail.com>
2019-02-10 13:20:02 +0300

    решение задачи 3

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createElement(node) {
+
+  if ((node === undefined) || (node === null) || (node === false)) {
+    return document.createTextNode('');
+  }
+
+  if(typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+
+  const element = document.createElement(node.name);
+
+  if (node.props) {
+    Object.keys(node.props).forEach(key => {
+      element.setAttribute(key, node.props[key]);
+    });
+  }
+
+   if (Array.isArray(node.childs)) {
+    element.appendChild(
+       node.childs.reduce((f, item) => {
+        f.appendChild(
+          createElement(item)
+        );
+
+        return f;
+      }, document.createDocumentFragment())
+    );
+  }
+
+  return element;
+
+};
