f1cb5b091550457882eab19b78fa9e3e0d60b054
Sergey Beketov <beketov.serg@gmail.com>
2019-02-16 17:02:33 +0300

    решение задачи 1

--- file-api/text-editor/js/main.js
+++ file-api/text-editor/js/main.js
@@ -18,17 +18,44 @@ class TextEditor {
   registerEvents() {
     const save = throttle( this.save.bind( this ), 1000 );
     this.contentContainer.addEventListener( 'input', save );
+    this.container.addEventListener('dragover', this.showHint.bind(this));
+    this.hintContainer.addEventListener('drop', this.loadFile.bind(this));
+    this.hintContainer.addEventListener('dragleave', this.hideHint.bind(this));
   }
   loadFile( e ) {
+    e.preventDefault();
+    this.hintContainer.classList.remove('text-editor__hint_visible');
+    const file = event.dataTransfer.files[0];
+    const fileTypeRegExp = /^text\/plain/;
+
+    if(fileTypeRegExp.test(file.type)) {
+      this.readFile( file );
+      this.setFilename( file.name );
+    } else {
+      alert('Файл не может быть загружен. Расширение файла доджно быть "txt"');
+    }
+
   }
   readFile( file ) {
+    const reader = new FileReader();
+    reader.addEventListener('load', event => {
+      this.load( event.target.result );
+      this.save();
+    });
+
+    reader.readAsText(file);
+
   }
   setFilename( filename ) {
     this.filenameContainer.textContent = filename;
   }
   showHint( e ) {
+    e.preventDefault();
+    this.hintContainer.classList.add('text-editor__hint_visible');
+
   }
   hideHint() {
+    this.hintContainer.classList.remove('text-editor__hint_visible');
   }
   load( value ) {
     this.contentContainer.value = value || '';
